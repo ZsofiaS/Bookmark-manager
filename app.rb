0a5feb54ec368981a054aa06983abddf36c4ec00
Zsofia Szonyi <szonyi.zsofia@gmail.com>
2020-05-06 20:44:12 +0100

    Add CSS to views

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ class BookmarkManager < Sinatra::Base
 
   post '/bookmarks' do
     Bookmark.create(url: params[:url], title: params[:title])
-    redirect '/bookmarks'
+    redirect '/'
   end
 
   get '/bookmarks' do
